78479806e8713f89f4f366f60ebd334d825ab364
8424bot <naoki.furansisco@gmail.com>
2022-07-07 11:06:49 +0900

    indexとMainActivity変更

--- android/app/src/main/kotlin/com/example/home_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/home_test/MainActivity.kt
@@ -4,3 +4,16 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+// 追加
+import io.flutter.plugins.urllauncher.UrlLauncherPlugin
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+	// 追加
+	UrlLauncherPlugin.registerWith(registrarFor("io.flutter.plugins.urllauncher.UrlLauncherPlugin"))
+    }
+}
