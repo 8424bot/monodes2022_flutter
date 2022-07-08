c313ec54cb70b9f0b9e214d8c256fd23c9fe45d2
e1920032 <e1920032@oit.ac.jp>
2022-07-08 13:42:01 +0900

    りどみにPaddingを追加

--- lib/view/home/Secondpage.dart
+++ lib/view/home/Secondpage.dart
@@ -17,116 +17,121 @@ class MySecondPage extends StatelessWidget {
           centerTitle: true,
           title: const Text("取扱説明書"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            //mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                "1.はじめに",
-                style: TextStyle(
-                  fontSize: 25,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              //mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  "1.はじめに",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-              Text(
-                "\n現在はテスト版です\n",
-                style: TextStyle(
+                Text(
+                  "\n現在はテスト版です\n",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "　これはものづくりデザイン思考実践演習Ⅰで作成したOIT梅田キャンパス生専用学生生活支援アプリです。\n"
+                  "不具合やご意見・ご要望等ありましたら作成者までご連絡ください。",
+                  style: TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "課題掲示板には不適切な投稿を防止する機能がありません。良識ある利用をよろしくお願いします。\n"
+                  "本アプリはより多くの人に使ってもらうことでより便利になっていきます。便利だと思ったらお友達に拡散していただけると幸いです。",
+                  style: TextStyle(
                     fontSize: 15,
-                    color: Colors.redAccent,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "　これはものづくりデザイン思考実践演習Ⅰで作成したOIT梅田キャンパス生専用学生生活支援アプリです。\n"
-                "不具合やご意見・ご要望等ありましたら作成者までご連絡ください。",
-                style: TextStyle(fontSize: 15),
-              ),
-              Text(
-                "課題掲示板には不適切な投稿を防止する機能がありません。良識ある利用をよろしくお願いします。\n"
-                "本アプリはより多くの人に使ってもらうことでより便利になっていきます。便利だと思ったらお友達に拡散していただけると幸いです。",
-                style: TextStyle(
-                  fontSize: 15,
+                  ),
                 ),
-              ),
-              Text(
-                "2.機能説明",
-                style: TextStyle(
-                  fontSize: 25,
+                Text(
+                  "2.機能説明",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const MyHomeExplainPage();
-                      }));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                      minimumSize: Size(120, 120),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MyHomeExplainPage();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        minimumSize: Size(120, 120),
+                      ),
+                      icon: Icon(Icons.home),
+                      label: Text('home'),
                     ),
-                    icon: Icon(Icons.home),
-                    label: Text('home'),
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const MyTodoExplainPage();
-                      }));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      minimumSize: Size(120, 120),
+                    SizedBox(
+                      width: 30,
                     ),
-                    icon: Icon(Icons.task_sharp),
-                    label: Text('Todo'),
-                  ),
-                ],
-              ),
-              Text("\n"),
-              Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const MySchoolExplainPage();
-                      }));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      minimumSize: Size(120, 120),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MyTodoExplainPage();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        minimumSize: Size(120, 120),
+                      ),
+                      icon: Icon(Icons.task_sharp),
+                      label: Text('Todo'),
                     ),
-                    icon: Icon(Icons.school),
-                    label: Text('school'),
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const MyMemoExplainPage();
-                      }));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.orange,
-                      minimumSize: Size(120, 120),
+                  ],
+                ),
+                Text("\n"),
+                Row(
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MySchoolExplainPage();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        minimumSize: Size(120, 120),
+                      ),
+                      icon: Icon(Icons.school),
+                      label: Text('school'),
                     ),
-                    icon: Icon(Icons.menu_book),
-                    label: Text('memo'),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      width: 30,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const MyMemoExplainPage();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                        minimumSize: Size(120, 120),
+                      ),
+                      icon: Icon(Icons.menu_book),
+                      label: Text('memo'),
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 30)),
+              ],
+            ),
           ),
         ));
   }
